4b1cb236419e684c793879dfb3cd4e051a782d97
wang.zhen <vq.momomiao>
2019-02-07 19:44:20 +0800

    commit

---
+++ src/main/java/com/wangzhen/spbsecuritydemo/SpbSecurityDemoApplication.java
@@ -0,0 +1,14 @@
+package com.wangzhen.spbsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbSecurityDemoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/wangzhen/spbsecuritydemo/SpbSecurityDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wangzhen.spbsecuritydemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpbSecurityDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
